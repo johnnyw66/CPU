47018b66382aa4b6d4409829b3710c987a002be3
John Wilson <john66@talk21.com>
2021-08-19 08:29:21 +0100

    Integrity check on control line and opcode defns

--- buildcontrolrom.py
+++ buildcontrolrom.py
@@ -3,8 +3,9 @@ controlWordSize = 24
 ACTIVEHIGH = 1
 ACTIVELOW = 0
 
-# Definition of our control control lines and which bit in the control word they
-# occupy
+# Definition of our  control lines and which bit in the control word they
+# occupy. We also define if the control bit is active low or active high.
+
 
 clLines = [
     {'key':"Cp", 'bit':23, 'active': ACTIVEHIGH, 'desc':"Enable PC count (inc PC)"},
@@ -26,9 +27,11 @@ clLines = [
     {'key':"Lp",  'bit':10, 'active': ACTIVEHIGH, 'desc':"Load PC (JUMP instructions) used with f1 and f0"},
     {'key':"f1",  'bit':9, 'active': ACTIVEHIGH, 'desc':"JUMP condition function bit 1"},
     {'key':"f0",  'bit':8, 'active': ACTIVEHIGH, 'desc':"JUMP condition Function bit 0 {00 -> Carry, 01 -> Non Zero, 10 -> Parity Odd, 11 --> Always}"},
+#    {'key':"Cp", 'bit':7, 'active': ACTIVEHIGH, 'desc':"TEST DUP"},
+
 #
     # Free control lines for use later
-    {'key':"U0",  'bit':7, 'active': ACTIVEHIGH,'desc':"Some Decription"},
+    #{'key':"U0",  'bit':7, 'active': ACTIVEHIGH,'desc':"Some Decription"},
     {'key':"U1",  'bit':6, 'active': ACTIVEHIGH, 'desc':"Some Decription"},
     {'key':"U2",  'bit':5, 'active': ACTIVEHIGH, 'desc':"Some Decription"},
     {'key':"U3",  'bit':4, 'active': ACTIVEHIGH, 'desc':"Some Decription"},
@@ -39,12 +42,27 @@ clLines = [
     {'key':"U7",  'bit':0, 'active': ACTIVEHIGH, 'desc':"Some Decription"},
 
 ]
-
+# Every microprocessor opcode instruction will have the
+# same three controlwords for T1,T2 and T3,
+# defined below
 fetchControlWords = [{'nLm'},{'nLm'},{'nLm'}]
+
+
+
+# Definition for each opcode in our microprocessor. We need to define
+# those control lines which are active in each cycle.
+# Note: The first three cycles T1, T2, T3
+# are already defined in our 'fetchControlWords' array.
 
 opcodes = [
     {'name':'NOP','bytecode': 0, 'control':
     [
+    ]},
+
+    {'name':'ADD','bytecode': 2,
+    'control':
+    [
+        {'Eu'}
     ]},
 
     {'name':'STA','bytecode': 1,
@@ -52,9 +70,14 @@ opcodes = [
     [
         {'Eu'}
     ]}
+
 ]
-def buildNOP():
+
+# Calculate the NOP microcode control word
+
+def buildNOPControlWord():
     NOPWord = 0
+
     for cl in clLines:
         NOPWord |= (cl['active']^1)<<cl['bit']
     return NOPWord
@@ -74,24 +97,49 @@ def buildControlWord(controlList):
     #print("buildControlWord",controlWord)
     return controlWord
 
+def checkInteg():
+    bitcheck = 0 ;
+    keySet = set()
+
+    for cl in clLines:
+        key = cl['key']
+        if (key in keySet):
+            raise Exception(f"Key {key} already defined for control line. Please check!")
+        keySet.add(key)
+
+        bitnum = 1<<cl['bit']
+        if (bitnum & bitcheck != 0):
+            raise Exception(f"Bit already defined for control line {cl['key']}. Please check!")
+        bitcheck |= bitnum
+
 def buildMicrocode():
-        #integCheck()
-    NOPWord = buildNOP()
+    checkInteg()
+    NOPWord = buildNOPControlWord()
     print(f"NOP Word {NOPWord:06x}")
     for op in opcodes:
-        print(op)
-        op['tsize'] = len(op['control'])
-        for tStateIndex,tStateCtrlst in enumerate(op['control']):
-            controlWord=buildControlWord(tStateCtrlst)
-            print(f"{tStateIndex} {controlWord:06x} {op['name']:10} number of tstates {op['tsize']}")
+        #print(op)
 
-        #print("Control Word {}", op['name'], op['bytecode'],controlWord)
-        # 24 bit
+        op['tsize'] = len(op['control'])
+        op['controlword'] = 0
+
+        for tStateIndex,tStateCtrlst in enumerate(op['control']):
+            op['controlword']=buildControlWord(tStateCtrlst)
+            #print(f"{tStateIndex} {op['controlword']:06x} {op['name']:10} number of tstates {op['tsize']}")
+
 
     return
 
+# opcodes.sort(reverse=True, key=myFunc)
+def sortKey(e):
+    return e['bytecode']
+
+def listMicrocode():
+    opcodes.sort(key=sortKey)
+    for op in opcodes:
+        print(op)
 
 try:
     buildMicrocode()
+    listMicrocode()
 except Exception as e:
-    print(f"oh no - check your control data or see a code doctor! {e}")
+    print(f"**ERROR** {e}")
